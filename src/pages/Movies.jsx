f9f028110ee94445caaa2aa1f82aa4c52973c610
Harsh1234567899 <pankhaniyaharsh222@gmail.com>
2025-07-31 19:36:32 +0530

    update movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,15 +5,15 @@ import BlureCircle from "../components/BlureCircle";
 
 const Movies = () => {
     return dummyShowsData.length > 0 ? (
-        <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-visible min-h-[80vh]'>
+        <div className='relative my-20 mb-40 px-4 sm:px-8 md:px-16 lg:px-40 xl:px-44 overflow-visible min-h-[80vh]'>
 
-            <BlureCircle top="150px" left="0px"/>
-            <BlureCircle top="100px" left="60%"/>
+            <BlureCircle top="10%" left="0%" />
+            <BlureCircle top="5%" left="60px" />
 
             <h1 className='text-lg font-medium my-4'>Now Showing</h1>
             <div className='flex flex-wrap max-sm:justify-center gap-8'>
-                {dummyShowsData.map((movie)=>(
-                    <MovieCard movie={movie} key={movie._id}/>
+                {dummyShowsData.map((movie) => (
+                    <MovieCard movie={movie} key={movie._id} />
                 ))}
             </div>
         </div>
@@ -21,7 +21,7 @@ const Movies = () => {
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-3xl font-bold text-center">No Movie Available</h1>
         </div>
-    )
-}
+    );
+};
 
-export default Movies
+export default Movies;
